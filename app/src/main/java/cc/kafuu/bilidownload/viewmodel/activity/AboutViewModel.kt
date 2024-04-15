76ed4efb8c1ee826fb7850ec56b07a1aa44e8dc7
hhyufan <1838248655@qq.com>
2024-04-16 00:29:01 +0800

    refactor: 删除不必要的注解文件，并拆分itemOnClickListener为各单一职责的Listener函数

--- app/src/main/java/cc/kafuu/bilidownload/viewmodel/activity/AboutViewModel.kt
+++ app/src/main/java/cc/kafuu/bilidownload/viewmodel/activity/AboutViewModel.kt
@@ -2,34 +2,30 @@ package cc.kafuu.bilidownload.viewmodel.activity
 
 import android.util.Log
 import cc.kafuu.bilidownload.common.core.CoreViewModel
-import cc.kafuu.bilidownload.model.AboutItemType
 
 class AboutViewModel: CoreViewModel() {
     companion object {
         private const val TAG = "AboutViewModel"
     }
-    fun itemOnClickListener(@AboutItemType position: Int) = when (position){
 
-        AboutItemType.ITEM_GRADE -> {
-            Log.d(TAG, "你点击了: 为我们评分")
-        }
 
-        AboutItemType.ITEM_INVITATION -> {
-            Log.d(TAG, "你点击了: 参与内测")
-        }
+    fun onClickInvitation() {
+        Log.d(TAG, "你点击了: 参与内测")
+    }
 
-        AboutItemType.ITEM_LICENSE -> {
-            Log.d(TAG, "你点击了: 开源组件许可")
-        }
+    fun onClickVersion() {
+        Log.d(TAG, "你点击了: 当前版本")
+    }
 
-        AboutItemType.ITEM_US -> {
-            Log.d(TAG, "你点击了: 关于我们")
-        }
+    fun onClickLicense() {
+        Log.d(TAG, "你点击了: 开源组件许可")
+    }
 
-        AboutItemType.ITEM_VERSION -> {
-            Log.d(TAG, "你点击了: 当前版本")
-        }
+    fun onClickAboutUS() {
+        Log.d(TAG, "你点击了: 关于我们")
+    }
 
-        else -> {Log.d(TAG, "你点击了: 未知错误")}
+    fun onClickGrade() {
+        Log.d(TAG, "你点击了: 给我们评分")
     }
 }
